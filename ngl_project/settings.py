eef67d7d38a7d35bf3346a0caed63db0d6513ace
Sanskar Bhardwaj <sanskarsrdav@gmail.com>
2025-06-08 02:21:34 +0530

    .

--- ngl_project/settings.py
+++ ngl_project/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l+qnpye@v__!l59tk+8@-tiq*!xg+-rzip=!^(1y*jscg1ro%s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
